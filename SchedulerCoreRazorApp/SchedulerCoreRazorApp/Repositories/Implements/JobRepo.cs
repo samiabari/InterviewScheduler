c80a7d97031414cb582c3d12df9b88b7a3d3cf2c
[samia <32800475+samiabari@users.noreply.github.com>
2024-07-14 00:47:44 +0800

    Update JobRepo.cs

--- SchedulerCoreRazorApp/SchedulerCoreRazorApp/Repositories/Implements/JobRepo.cs
+++ SchedulerCoreRazorApp/SchedulerCoreRazorApp/Repositories/Implements/JobRepo.cs
@@ -1,69 +1,26 @@
-﻿using SchedulerCoreRazorEntityApp.DAL;
+﻿using Microsoft.EntityFrameworkCore;
+using SchedulerCoreRazorEntityApp.DAL;
 using SchedulerCoreRazorEntityApp.Models;
 using SchedulerCoreRazorEntityApp.Repositories.Interface;
+using System;
 
 namespace SchedulerCoreRazorEntityApp.Repositories.Implements
 {
-    public class JobRepo : IJobRepo
+    public class JobRepo : GenericRepo<Job>, IJobRepo
     {
-        private readonly AppDbContext appDbContext;
-        public JobRepo(AppDbContext _appDbContext) 
-        { 
-            this.appDbContext = _appDbContext;
-        }
-        public Job AddJob(Job job)
+        public JobRepo(AppDbContext _appDbContext) : base(_appDbContext)
         {
-            var result = appDbContext.Jobs.Add(job);
-            appDbContext.SaveChanges();
-            return result.Entity;
+
         }
 
-        public Job UpdateJob(int id, Job job)
+        public async Task<IEnumerable<Job>> GetAllPendingJobs(int status_id)
         {
-            var result = appDbContext.Jobs.Find(id);
-            
-            if (result == null)
+            var obj = await appDbContext.Jobs.Where(j=> j.Status_id== status_id).ToListAsync();
+            if (obj == null)
             {
-                throw new Exception("No Data found to be updated");
+                throw new Exception("No Data found with this status.");
             }
-            var updatedJob = appDbContext.Jobs.Update(job);
-            appDbContext.SaveChanges();
-            return updatedJob.Entity;
-
+            return obj;
         }
-        public bool DeleteJob(int id)
-        {
-            var result = appDbContext.Jobs.Find(id);
-
-            if (result == null)
-            {
-                throw new Exception("No Data found to be updated");
-            }
-
-            try
-            {
-                appDbContext.Jobs.Remove(result);
-                appDbContext.SaveChanges();
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        public List<Job> GetAllJob()
-        {
-            var jobs = appDbContext.Jobs.ToList();
-            return jobs;
-        }
-
-        public Job GetJobById(int id)
-        {
-            var job = appDbContext.Jobs.Find(id);
-            return job;
-        }
-
-       
     }
 }
