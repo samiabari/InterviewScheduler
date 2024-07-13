36fd7aa27c64592e6e68c827a5f2db1ea5bbe47e
[samia <32800475+samiabari@users.noreply.github.com>
2024-07-13 18:14:31 +0800

    Interface added for job repo

---
+++ SchedulerCoreRazorApp/SchedulerCoreRazorApp/Repositories/Interface/IJobRepo.cs
@@ -0,0 +1,14 @@
+﻿using SchedulerCoreRazorEntityApp.Models;
+
+namespace SchedulerCoreRazorEntityApp.Repositories.Interface
+{
+    public interface IJobRepo
+    {
+        Job AddJob(Job job);
+        Job UpdateJob(Job job);
+        bool DeleteJob(int id);
+        Job GetJobById(int id);
+        Job GetAllJob();
+        
+    }
+}
