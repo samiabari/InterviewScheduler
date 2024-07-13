c0944cddda770b4e5074844841d99086486b66d5
[samia <32800475+samiabari@users.noreply.github.com>
2024-07-13 20:43:27 +0800

    Generic Repo added and Implemented

---
+++ SchedulerCoreRazorApp/SchedulerCoreRazorApp/Repositories/Interface/IGenericRepo.cs
@@ -0,0 +1,15 @@
+﻿using SchedulerCoreRazorEntityApp.Models;
+
+namespace SchedulerCoreRazorEntityApp.Repositories.Interface
+{
+    public interface IGenericRepo<T> where T : class
+    {
+        T Add(T t);
+        T Update(int id, T t);
+        bool Delete(int id);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+
+        bool Save();
+    }
+}
